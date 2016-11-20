53e97d06b9a624167e45040b94f41d67cc26c744
Mark Miller <Mark Miller@192.168.1.105>
2016-11-20 16:25:35 -0500

    Added printBoard() & Board() for testing the piece and pawn class

--- src/chessengine/Board.java
+++ src/chessengine/Board.java
@@ -10,5 +10,17 @@ package chessengine;
  * @author Mark Miller
  */
 public class Board {
-    
+    Piece [][] board = new Piece[1][1];
+  public Board(){
+      board[0][0] = new Pawn(0,0,true);
+      board[1][0] = new Pawn(1,0,true);
+  } 
+  
+  public void printBoard(){
+      for(int i = 0; i < board.length; i++){
+          for(int j = 0; j < board[j].length; j++){
+              System.out.println(board[i][j]);
+          }
+      }
+  }
 }
