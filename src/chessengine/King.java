073b3ecb94aebe9c5ae0e91b3e44257a078a6660
Mark Miller <Mark Miller@192.168.1.105>
2016-11-20 18:21:03 -0500

    Added King class

---
+++ src/chessengine/King.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessengine;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mark Miller
+ */
+public class King extends Piece {
+
+    public King(Color c) {
+        super(c);
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isValidMove(int row, int column, Board board) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<int[]> getValidMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
