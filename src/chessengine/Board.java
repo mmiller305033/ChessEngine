0d018d50eda36914e5a5c352da30d34b1b805b2f
Mark Miller <Mark Miller@192.168.1.105>
2016-11-20 16:58:28 -0500

    testing Boards movePiece()

--- src/chessengine/Board.java
+++ src/chessengine/Board.java
@@ -10,17 +10,25 @@ package chessengine;
  * @author Mark Miller
  */
 public class Board {
-    Piece [][] board = new Piece[1][1];
+    Piece [][] board = new Piece[2][2];
   public Board(){
-      board[0][0] = new Pawn(0,0,true);
       board[1][0] = new Pawn(1,0,true);
+      board[1][1] = new Pawn(1,1,true);
   } 
   
   public void printBoard(){
       for(int i = 0; i < board.length; i++){
-          for(int j = 0; j < board[j].length; j++){
-              System.out.println(board[i][j]);
+          for(int j = 0; j < board[0].length; j++){
+              System.out.print(board[i][j]);
           }
+          System.out.println("");
       }
+  }
+  
+  public void movePiece(int pieceRow, int pieceColumn, int newRow, int newColumn){
+      board[newRow][newColumn] = board[pieceRow][pieceColumn];
+       
+      board[pieceRow][pieceColumn] = null;
+       
   }
 }
