1c189d8e3480c4005d158d5f7c318d2ea18cec9a
Mark Miller <Mark Miller@192.168.1.105>
2016-11-20 18:29:09 -0500

    Class contains: color, piece, Row, and Column.

--- src/chessengine/BoardHistory.java
+++ src/chessengine/BoardHistory.java
@@ -5,10 +5,37 @@
  */
 package chessengine;
 
+import java.awt.Color;
+
 /**
  *
  * @author Mark Miller
  */
 public class BoardHistory {
+    private Color color;
+    private Piece piece;
+    private int Row;
+    private int Column;
+    public BoardHistory(Color c, Piece p, int row, int column){
+        color = c;
+        piece = p;
+        Row = row;
+        Column = column;
+    }
     
+    public Color getColor(){
+        return color;
+    }
+    
+    public Piece getPiece(){
+        return piece;
+    }
+    
+    public int getRow(){
+        return Row;
+    }
+    
+    public int getColumn(){
+        return Column;
+    }
 }
